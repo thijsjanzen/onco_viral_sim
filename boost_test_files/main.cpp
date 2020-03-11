d7ff0046d0f5cd4f01d1d28b6885c8ffffba9811
Thijs Janzen <thijsjanzen@gmail.com>
2020-03-11 15:07:57 +0100

    add tests

--- boost_test_files/main.cpp
+++ boost_test_files/main.cpp
@@ -5,15 +5,54 @@
 #include "../Simulation/simulation.hpp"
 #include "../Simulation/analysis.hpp"
 
-
-
-
-
-BOOST_AUTO_TEST_CASE( check_if_dead )
+BOOST_AUTO_TEST_CASE( check_full_2 )
 {
   Param all_parameters;
   all_parameters.sq_num_cells = 100;
   all_parameters.use_voronoi_grid = false;
+  all_parameters.birth_infected = 0.2f;
+  all_parameters.death_infected = 2.0f;
+
+  // GROW SETUP
+  all_parameters.start_setup = full;
+
+  std::string outcome = do_analysis(all_parameters);
+  BOOST_CHECK_EQUAL(outcome, "B");
+
+  all_parameters.use_voronoi_grid = true;
+  outcome = do_analysis(all_parameters);
+  BOOST_CHECK_EQUAL(outcome, "B");
+}
+
+BOOST_AUTO_TEST_CASE( check_grow_2 )
+{
+  Param all_parameters;
+  all_parameters.sq_num_cells = 100;
+  all_parameters.use_voronoi_grid = false;
+  all_parameters.birth_infected = 0.2f;
+  all_parameters.death_infected = 2.0f;
+
+  // GROW SETUP
+  all_parameters.start_setup = grow;
+  all_parameters.maximum_time = 10000;
+  all_parameters.time_adding_cancer = 1000;
+  all_parameters.time_adding_virus = 2000;
+
+  std::string outcome = do_analysis(all_parameters);
+  BOOST_CHECK_EQUAL(outcome, "B");
+
+  all_parameters.use_voronoi_grid = true;
+  outcome = do_analysis(all_parameters);
+  BOOST_CHECK_EQUAL(outcome, "B");
+}
+
+BOOST_AUTO_TEST_CASE( check_basic_functionality )
+{
+  Param all_parameters;
+  all_parameters.sq_num_cells = 100;
+  all_parameters.use_voronoi_grid = false;
+
+  // set birth infected and birth cancer to 0
   all_parameters.birth_infected = 0.f;
   all_parameters.birth_cancer = 0.f;
 
@@ -46,3 +85,11 @@ BOOST_AUTO_TEST_CASE( check_grow )
   outcome = do_analysis(all_parameters);
   BOOST_CHECK_EQUAL(outcome, "A");
 }
+
+
+
+
+
+
+
+
